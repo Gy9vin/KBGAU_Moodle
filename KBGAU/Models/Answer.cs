26bc29cd825f00fd55d108440ba45df3b33b6c42
gy9vin <gy9vin@gmail.com>
2024-12-13 00:20:05 +0300

    /

--- KBGAU/Models/Answer.cs
+++
@@ -1,8 +0,0 @@
-public class Answer
-{
-    public int Id { get; set; }
-    public int QuestionId { get; set; }
-    public string Text { get; set; }
-    public bool IsCorrect { get; set; }
-    public string? MatchText { get; set; } // Сделано nullable
-}
